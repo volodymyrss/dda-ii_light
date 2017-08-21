7583e1770b390a3c4666f5d54cab3245d7ae3c72
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2017-08-21 14:16:43 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='ii_light',
+    version='1.0',
+    py_modules=['ii_light',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    #long_description=open('README.md').read(),
+)
